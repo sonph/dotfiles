b4eec63ba22a5230dcd3da3dbd9510fcdd1b3e36
Son A. Pham <sp@sonpham.me>
2016-02-27 16:47:16 -0800

    remove spf13 setup scripts; merge symlink.sh into setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-sh ./symlink.sh
-sh ./spf13-vim.sh
+
+ANYKEY='  >>  Press any key to continue or C-c to cancel...'
+
+# symlink files
+echo '  >>  Symlinking files...'
+echo "$ANYKEY"; read
+
+DIR="home"
+
+for file in $(ls $DIR); do
+    ln -s $PWD/$DIR/$file $HOME/.${file}
+done
+
+ln -s $PWD/bin $HOME/bin
+
+# setup vim
+echo '  >>  Setting up vim...'
+echo "$ANYKEY"; read
+
+vim $HOME/.vimrc.plugins -c "PluginInstall"
+
--- symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="home"
-
-for file in $(ls $DIR); do
-    ln -s $PWD/$DIR/$file $HOME/.${file}
-done
-
-ln -s $PWD/bin $HOME/bin
