c2a847f7a37b6305a2fefc8ced43cd3b5123b821
Son A. Pham <sp@sonpham.me>
2017-02-25 15:18:18 -0800

    refactor install.sh

--- kali/install.sh
+++ kali/install.sh
@@ -1,41 +1,220 @@
 #!/bin/bash
 # ------------------------------------------------
-# Script to install additional stuffs on a new kali install.
+# Script to install additional stuffs on a new linux install.
+# Assume Debian-based and XFCE.
 # ------------------------------------------------
 
-apt-get update
-apt-get install -y neovim python3 python3-pip apt-file ttyrec zsh tmux xcape \
-  htop nmon tor torbrowser-launcher xbindkeys xbindkeys-config lynx lm-sesors
-apt-get autoremove
+DOTFILES_DIR="$HOME/.files"
+# TODO: set if not set
+XDG_CONFIG_HOME="$HOME/.config"
+[[ ! -d "$XDG_CONFIG_HOME" ]] && mkdir -p "$XDG_CONFIG_HOME"
+CODE_DIR='/code'
+[[ ! -d "$CODE_DIR" ]] && mkdir -p "$CODE_DIR"
 
-pip3 install neovim
+common_bin_exists() {
+  command -v "$1" 2>&1 > /dev/null
+  # By default return code is from the last command.
+}
 
-searchsploit -u
+common_bin_exists 'apt-get' && apt-get update
 
-mkdir /code
-cd /code
-git clone https://github.com/sonph/onehalf
-source onehalf/gnome-terminal/onehalfdark.sh
+common_install_pkg() {
+  if common_bin_exists 'apt-get'; then
+    apt-get install -y $@
+  elif common_bin_exists 'yum'; then
+    yum install $@
+  else
+    err 'Either apt-get or yum not found.'
+    return 1
+  fi
+}
 
-chsh -s /usr/bin/zsh root
-touch ~/.zprofile
-echo "setxkbmap -option 'caps:ctrl_modifier'" >> ~/.zprofile
-echo "xcape -e 'Caps_Lock=Escape'" >> ~/.zprofile
+misc-install() {
+  common_install_pkg ttyrec apt-file software-properties-common lm-sensors
+  common_install_pkg zsh tmux xcape htop nmon xbindkeys xbindkeys-config
+  common_install_pkg ctags cmake autoconf
+  common_install_pkg lynx
+  common_bin_exists 'apt-get' && apt autoremove
+}
 
-# tor
-torbrowser-launcher
+err() {
+  >&2 echo $@
+}
 
-systemctl start tor
-systemctl enable tor
 
-# nvm
-apt-get install -y build-essential libssl-dev
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install --lts
+curl-install() {
+  common_bin_exists 'curl' && return
+  common_install_pkg 'curl'
+}
 
-npm install -g diff-so-fancy
+nvm-install() {
+  # test
+  common_bin_exists 'nvm' && return
+  # deps
+  curl-install
+  # install
+  common_install_pkg build-essential libssl-dev
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+}
+
+npm-install() {
+  common_bin_exists 'npm' && return
+  nvm install --lts
+}
+
+diff-so-fancy-install() {
+  # test
+  common_bin_exists 'diff-so-fancy' && return
+  # deps
+  npm-install
+  # install
+  npm install -g diff-so-fancy
+}
+
+git-install() {
+  # test
+  common_bin_exists 'git' && return
+  # deps
+  diff-so-fancy-install;
+  # install
+  common_install_pkg 'git'
+}
+
+python3-install() {
+  # test
+  common_bin_exists 'python3' && return
+  # install
+  common_install_pkg 'python3'
+}
+
+pip3-install() {
+  # test
+  common_bin_exists 'pip3' && return
+  # deps
+  python3-install
+  # install
+  common_install_pkg 'python3-pip'
+}
+
+neovim() {
+  local URL='https://neovim.io/doc/'
+  # test
+  command -v nvim 2>&1 > /dev/null && return
+  # deps
+  pip3-install; git-install
+  # install
+  apt-get install -y neovim
+  pip3 install neovim
+
+  local VIM_DIR="$DOTFILES_DIR/home/vim"
+  local NVIM_CONFIG_DIR="$XDG_CONFIG_HOME/nvim"
+    [[ ! -e "$NVIM_CONFIG_DIR" ]] && ln -s "$VIM_DIR" "$NVIM_CONFIG_DIR"
+
+  local VIM_DEIN_REPOS_DIR="$VIM_DIR/bundle/repos"
+  local VIM_DEIN_DIR="$VIM_DEIN_REPOS_DIR/github.com/Shougo/dein.vim"
+  mkdir -p "$VIM_DEIN_DIR"
+  git clone https://github.com/Shougo/dein.vim "$VIM_DEIN_DIR"
+  nvim -c ":call dein#install()"
+}
+
+gnome-terminal() {
+  if common_bin_exists; then
+  else
+    common_install_pkg 'gnome-terminal'
+  fi
+  pushd "$CODE_DIR" 2>&1 > /dev/null
+  git clone 'https://github.com/sonph/onehalf'
+  source onehalf/gnome-terminal/onehalfdark.sh
+  source onehalf/gnome-terminal/onehalflight.sh
+  popd 2>&1 > /dev/null
+}
+
+searchsploit-update() {
+  # update
+  searchsploit -u
+}
+
+tor-browser-install() {
+  local URL="https://torproject.org/projects/torbrowser.html.en"
+  tor-install
+  common_install_pkg 'torbrowser-launcher'
+  torbrowser-launcher
+  echo "If the launcher fails to download, visit $URL"
+}
+
+tor-install() {
+  if ! common_bin_exists 'tor'; then
+    common_install_pkg 'tor'
+  fi
+  systemctl start tor
+  systemctl enable tor
+  tor-browser-install
+}
+
+arc-theme-install() {
+  local URL='https://github.com/horst3180/arc-theme'
+  # test: TODO
+  # install
+  if [[ $# -eq 0 ]]; then
+    # with transparency
+    common_install_pkg 'arc-theme'
+  else
+    # manual build with options (see URL for options)
+    # --disable-{transparency,cinnamon,gnome-shell,unity,...}
+    common_install_pkg autoconf automake pkg-config libgtk-3-dev \
+        gnome-themes-standard gtk2-engines-murrine
+    git-install
+    pushd "$CODE" 2>&1 > /dev/null
+    git clone "$URL"
+    pushd "$CODE/arc-theme" 2>&1 > /dev/null
+    ./autogen.sh --prefix=/usr --disable-transparency $@
+    sudo make install
+    popd 2>&1 > /dev/null
+    popd 2>&1 > /dev/null
+  fi
+
+  common_bin_exists 'xfce4-settings-manager' && xfce4-settings-manager
+  echo "Select Arc (Dark|Darker) theme in Appearance and Window Manager"
+}
+
+chromium-install() {
+  
+}
 
 # reaver
-# flux
+
+flux-install() {
+  local URL='https://github.com/xflux-gui/xflux-gui'
+  # test
+  common_bin_exists 'fluxgui' && return
+  # deps
+  git-install
+  # install
+  common_install_pkg python-appindicator python-xdg python-pexpect python-gconf \
+      python-gtk2 python-glade2 libxxf86vm1
+  pushd /tmp 2>&1 > /dev/null
+  git clone "$URL"
+  cd xflux-gui
+  python download-xflux.py
+  sudo python setup.py install
+  popd 2>&1 > /dev/null
+  echo 'To start flux, run `fluxgui`'
+
+  # local URL='https://launchpad.net/~nathan-renniewaldock/+archive/ubuntu/flux'
+  # if common_bin_exists 'apt-get'; then
+    # common_bin_exists 'add-apt-repository' || common_install_pkg 'software-properties-common'
+    # # TODO: not working!
+    # sudo add-apt-repository ppa:nathan-renniewaldock/flux
+    # sudo apt-get update
+    # sudo apt-get install fluxgui
+  # else
+    # err 'flux-install: Yum not supported'
+  # fi
+}
+
+user-setup() {
+  common_bin_exists 'zsh' && chsh -s $(command -v zsh) root
+}
+
