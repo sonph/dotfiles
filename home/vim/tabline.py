4d559bec2addaf1459818ca4699653ce172f0755
Son A. Pham <sp@sonpham.me>
2017-03-06 16:59:42 -0800

    tabline.py refactor

--- home/vim/tabline.py
+++ home/vim/tabline.py
@@ -2,9 +2,9 @@
 
 # TODO(sonph): Clean up and convert into a python plugin.
 
-import vim
 import os
 import re
+import vim
 
 EXCLUDE = [
     'NERD_tree.*',
@@ -12,7 +12,7 @@ EXCLUDE = [
     '__Tagbar.*',
 ]
 
-MIN_TAB_TITLE_LENGTH = 20
+MIN_TAB_TITLE_LENGTH = 25
 
 DEBUG = False
 
@@ -23,12 +23,6 @@ def debug(s):
 def get_tab(tabpage):
   tab_parts = []
   tabpage_is_current = (tabpage.number == vim.current.tabpage.number)
-
-  # With tab number.
-  # tab_parts.append('%%#TabLine%s#%%%dT %d %%#TabLine%s#' %
-  #     ('Sel' if tabpage_is_current else '', tabpage.number, tabpage.number, 'Sel' if tabpage_is_current else ''))
-  prepend = ('%%#TabLine%s#%%T%%#TabLine%s#' %
-      ('Sel' if tabpage_is_current else '', 'Sel' if tabpage_is_current else ''))
 
   # File names.
   fnames = []
@@ -41,23 +35,32 @@ def get_tab(tabpage):
     else:
       fname_parts = []
       fname_parts.append('[' if window_is_current else ' ')
-      fname_parts.append(fname if fname else 'new')
-      if vim.call('getbufvar', window.buffer.name, '&modified'):
-        fname_parts.append('*')
+
+      fname_buf_type = vim.call('getbufvar', window.buffer, '&buftype')
+      if fname_buf_type == 'help':
+        fname_parts.append('H:%s' % fname.split('.')[0])
+      elif fname_buf_type == 'quickfix':
+        fname_parts.append('Q')
+      else:
+        fname_parts.append(fname if fname else 'new')
+        if vim.call('getbufvar', window.buffer, '&modified'):
+          fname_parts.append('*')
+
       fname_parts.append(']' if window_is_current else ' ')
       fnames.append(''.join(fname_parts))
   tab_parts.append(''.join(fnames))
 
+  # With tab number.
+  # prepend = ('%%#TabLine%s#%%%dT %d %%#TabLine%s#' %
+  #     ('Sel' if tabpage_is_current else '', tabpage.number, tabpage.number, 'Sel' if tabpage_is_current else ''))
+  prepend = ('%%#TabLine%s#%%T%%#TabLine%s#' %
+      ('Sel' if tabpage_is_current else '', 'Sel' if tabpage_is_current else ''))
+
   # Padding
   tab_str = ''.join(tab_parts)
-  if len(tab_str) >= MIN_TAB_TITLE_LENGTH:
-    debug('no padding')
-    return '%s %s ' % (prepend, tab_str)
-  else:
-    debug('padding')
-    padding_left = (MIN_TAB_TITLE_LENGTH - len(tab_str)) // 2
-    padding_right = MIN_TAB_TITLE_LENGTH - len(tab_str) - padding_left
-    return '%s%s%s%s' % (prepend, ' ' * padding_left, tab_str, ' ' * padding_right)
+  padding_left = max((MIN_TAB_TITLE_LENGTH - len(tab_str)) // 2, 1)
+  padding_right = max(MIN_TAB_TITLE_LENGTH - len(tab_str) - padding_left, 1)
+  return '%s%s%s%s' % (prepend, ' ' * padding_left, tab_str, ' ' * padding_right)
 
 def get_line():
   line_parts = []
@@ -65,7 +68,7 @@ def get_line():
   for tab in vim.tabpages:
     line_parts.append(get_tab(tab))
   line_parts.append('%#TabLineFill#%T%=%#TabLineFill#%999X')
-
+  line_parts.append('%%#TablineSel# %s ' % os.path.basename(os.getcwd()))
   return ''.join(line_parts)
 
 tabline = get_line()
