e94e416695f7e0125e4ff76ffc9cd703b04d8725
Son A. Pham <sonph@users.noreply.github.com>
2017-04-26 00:45:48 -0700

    common_utils.sh include guards

--- bin/common_utils.sh
+++ bin/common_utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Common functions for testing scripts.
-# TODO(sonph): maybe use include guards
+if [[ -z "$SONPH_COMMON_UTILS" ]]; then
+SONPH_COMMON_UTILS=yes  # Include guard.
 RED="$(tput setaf 1)"
 GREEN="$(tput setaf 2)"
 BLUE="$(tput setaf 4)"
@@ -8,4 +9,4 @@ RESET="$(tput sgr0)"
 info() { echo "[${BLUE}INFO${RESET}] $@"; }
 fail() { echo "[${RED}FAIL${RESET}] $@"; }
 ok() { echo "[ ${GREEN}OK${RESET} ] $@"; }
-
+fi
