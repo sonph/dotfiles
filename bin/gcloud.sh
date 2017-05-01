d85d99d5b97808a42db4c01255960af56a964efd
Son A. Pham <sonph@users.noreply.github.com>
2017-05-01 01:22:07 -0700

    add bin/gcloud.sh for common gcloud stuff

---
+++ bin/gcloud.sh
@@ -0,0 +1,25 @@
+# Helper functions for gcloud.
+
+# Usage: gcloud.sh ssh
+
+USERNAME='sp'
+COMPUTE_INSTANCE='instance-1'
+SSH_KEY_FILE="$HOME/.ssh/gcp"
+
+function ssh() {
+  echo "gcloud compute ssh $USERNAME@$COMPUTE_INSTANCE --ssh-key-file=$SSH_KEY_FILE"
+  gcloud compute ssh $USERNAME@$COMPUTE_INSTANCE --ssh-key-file=$SSH_KEY_FILE
+}
+
+function copy_files() {
+  echo "NOTE: remote path MUST be wrapped in '' to avoid automatic directory expansion, for example ~"
+  echo "gcloud compute copy-files $USERNAME@$COMPUTE_INSTANCE:$1 $2 --ssh-key-file=$SSH_KEY_FILE"
+  gcloud compute copy-files $USERNAME@$COMPUTE_INSTANCE:"$1" "$2" --ssh-key-file=$SSH_KEY_FILE
+}
+
+if [ $# -eq 0 ]; then
+  echo $(compgen -A function) | sed 's/\(fail\|info\|ok\) //g'
+  exit 0
+fi
+$@
+
