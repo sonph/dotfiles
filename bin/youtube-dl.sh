6cc546180b0b94087006fee476676644af7753c0
Son A. Pham <sonph@users.noreply.github.com>
2017-05-01 01:48:33 -0700

    youtube-dl.sh

---
+++ bin/youtube-dl.sh
@@ -0,0 +1,50 @@
+# Guide: https://github.com/rg3/youtube-dl
+#
+# Usage: youtube-dl.sh download-audio-playlist "https://youtube.com/playlist?list=..."
+#
+# Note:
+#   - Playlist must not be private (public or unlisted)
+#   - Youtube-dl should be up to date before use (youtube-dl.sh update)
+
+source common_utils.sh
+
+function update() {
+  info "Updating youtube-dl: youtube-dl -U"
+  youtube-dl -U
+}
+
+function download_audio_playlist() {
+  local playlist="${1}"
+  info "MAKE SURE playlist is accessible through the url (public or unlisted)"
+  if [[ -z "$playlist" ]]; then
+    fail "Must provide a playlist url"
+    echo "Usage: youtube-dl.sh download_audio_playlist \"https://youtube.com/playlist?list=...\" [output_dir]"
+    return 1
+  fi
+  info "Playlist: $playlist"
+  local output_dir=${2:-$(mktemp -d youtube-dl.sh.XXXX)}
+  info "Output dir: $output_dir"
+
+  update
+  pushd $output_dir
+  youtube-dl \
+      --extract-audio \
+      --audio-format mp3 \
+      --audio-quality 2 \
+      --add-metadata \
+      --yes-playlist \
+      $playlist
+}
+
+function install_mac() {
+  local cmd="brew install youtube-dl ffmpeg"
+  info $cmd
+  $cmd
+}
+
+if [ $# -eq 0 ]; then
+  echo $(compgen -A function) | sed 's/\(fail\|info\|ok\) //g'
+  exit 0
+fi
+$@
+
